5bee71f8e75b1f3061e657a875ef4aa99d843634
Jakub Gorzenski <kuba.gorzenski@gmail.com>
2025-07-24 13:46:29 +0200

    renamed manifest.txt to file_list.txt, a.wasm to game.wasm. index.html now downloads files base on file_list.txt

--- Build_WASM.bat
+++ Build_WASM.bat
@@ -7,13 +7,13 @@ cls
 
 cd tmp
 del ..\a.zip
-echo a.wasm > manifest.txt
+echo game.wasm > file_list.txt
 
 FOR %%i IN (%*) DO (
     IF "%%~xi"==".c" (
         clang -cc1 -Ofast -emit-llvm-bc -triple=wasm32-unknown-unknown-wasm -std=c23 %%i -o a.bc -Wall -Wextra -pedantic
     ) ELSE (
-        echo %%~nxi >> manifest.txt
+        echo %%~nxi >> file_list.txt
         7z a ..\a.zip %%i > nul
     )
 )
@@ -23,10 +23,10 @@ llvm-link a.bc b.bc -o a.bc
 opt -O3 a.bc -o a.bc
 llc -O3 -filetype=obj a.bc -o a.o
 rem wasm-ld a.o -o a.wasm --no-entry --export-dynamic --allow-undefined 
-wasm-ld a.o -o a.wasm --strip-all --allow-undefined
+wasm-ld a.o -o game.wasm --strip-all --allow-undefined
 rem --import-memory
 
-7z a ..\a.zip ..\BRUH\WASM\index.html a.wasm manifest.txt > nul
+7z a ..\a.zip ..\BRUH\WASM\index.html game.wasm file_list.txt > nul
 
 cd ..
 
